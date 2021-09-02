295f1c3efeaaf96ff7b2cdbbae0c25167b8f1c74
Gopeshwar Kumawat <gopeshwark@gmail.com>
2021-09-02 12:32:53 +0530

    Update Student.js

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Paper, Typography } from "@material-ui/core";
+import { useParams } from "react-router-dom";
+import { studentData } from "../data";
 
 const Student = () => {
-  return <div>Student</div>;
+  const params = useParams();
+  const [student, setStudent] = useState({});
+
+  useEffect(() => {
+    const currentStudent = studentData.find(
+      (student) => student.id == params.id
+    );
+
+    setStudent(currentStudent);
+  }, [params]);
+  console.log(student);
+  const { id, firstName, lastName, phone, email, address } = student;
+  return (
+    <Paper key={id}>
+      <Typography variant="h2" gutterBottom>
+        {firstName} {lastName}
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        {phone}
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        {email}
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        {address}
+      </Typography>
+    </Paper>
+  );
 };
 
 export default Student;
