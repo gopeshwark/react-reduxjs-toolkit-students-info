f4dfadc95af68e18efa5d24df3ea60e25d93f505
Gopeshwar Kumawat <gopeshwark@gmail.com>
2021-09-02 13:29:23 +0530

    Frontend Added

--- src/components/AddStudent.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AddStudent = () => {
-  return <div>Add Student</div>;
-};
-
-export default AddStudent;
--- src/components/EditStudent.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EditStudent = () => {
-  return <div>Edit Students Page</div>;
-};
-
-export default EditStudent;
